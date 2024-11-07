d3b6bfa380ee1b83e87c6ab885d0a8b69d827866
Raphael Stefanini <raphael@rphlo.com>
2024-11-07 11:51:38 +0200

    Reduce JXL lib to minimum required

--- routechoices/lib/jxl.py
+++ routechoices/lib/jxl.py
@@ -1,63 +1,8 @@
-from io import BytesIO
-
-from PIL import Image, ImageFile
-from pillow_jxl import Decoder, Encoder
+from PIL import Image
+from pillow_jxl import Encoder
 
 _VALID_JXL_MODES = {"RGB", "RGBA", "L", "LA"}
-
-
-def _accept(data):
-    return (
-        data[:2] == b"\xff\x0a"
-        or data[:12] == b"\x00\x00\x00\x0c\x4a\x58\x4c\x20\x0d\x0a\x87\x0a"
-        or data[4:7] == b"JXL"
-    )
-
-
-class JXLImageFile(ImageFile.ImageFile):
-    format = "JXL"
-    format_description = "Jpeg XL image"
-    __loaded = -1
-    __frame = 0
-
-    def _open(self):
-        self.fc = self.fp.read()
-        self._decoder = Decoder()
-
-        self.jpeg, self._jxlinfo, self._data, icc_profile = self._decoder(self.fc)
-        if self.jpeg:
-            with Image.open(BytesIO(self._data)) as im:
-                self._data = im.tobytes()
-        self._size = (self._jxlinfo.width, self._jxlinfo.height)
-        self.rawmode = self._jxlinfo.mode
-        self.info["icc_profile"] = icc_profile
-        self._mode = self.rawmode
-        self.tile = []
-
-    def load(self):
-        if self.__loaded != self.__frame:
-            if self._data is None:
-                EOFError("no more frames")
-
-            self.__loaded = self.__frame
-
-            if self.fp and self._exclusive_fp:
-                self.fp.close()
-            self.fp = BytesIO(self._data)
-            self.tile = [("raw", (0, 0) + self.size, 0, self.rawmode)]
-
-        return super().load()
-
-    # may be defined for contained formats
-    def load_seek(self, pos):
-        pass
-
-    # may be defined for blocked formats (e.g. PNG)
-    # def load_read(self, bytes):
-    #     pass
-
-    def tell(self):
-        return self.__frame
+JXL_FORMAT = "JXL"
 
 
 def _save(im, fp, filename, save_all=False):
@@ -94,7 +39,6 @@ def _save(im, fp, filename, save_all=False):
 
 
 def register_jxl_opener():
-    Image.register_open(JXLImageFile.format, JXLImageFile, _accept)
-    Image.register_save(JXLImageFile.format, _save)
-    Image.register_mime(JXLImageFile.format, "image/jxl")
-    Image.register_extension(JXLImageFile.format, ".jxl")
+    Image.register_save(JXL_FORMAT, _save)
+    Image.register_mime(JXL_FORMAT, "image/jxl")
+    Image.register_extension(JXL_FORMAT, ".jxl")
