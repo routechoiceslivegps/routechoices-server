6ee43bfe7a3f65fca3652b5235a5ea199f40b388
Raphael Stefanini <raphael@rphlo.com>
2024-11-19 15:38:34 +0200

    Refactor xexun tcp handler

--- routechoices/lib/tcp_protocols/xexun.py
+++ routechoices/lib/tcp_protocols/xexun.py
@@ -1,7 +1,6 @@
 import re
 
 import arrow
-from django.core.exceptions import ValidationError
 
 from routechoices.lib.helpers import random_key
 from routechoices.lib.tcp_protocols.commons import (
@@ -23,91 +22,68 @@ class XexunConnection:
         self.db_device = None
         self.logger = logger
 
-    async def start_listening(self):
-        print(f"Start listening from {self.address}")
-        imei = None
-        try:
-            data_raw = ""
-            while not data_raw:
-                data_bin = await self.stream.read_bytes(255, partial=True)
-                data_raw = data_bin[:-2].decode("ascii").strip()
-                data_raw = re.search(r"G[PN]RMC,.+", data_raw).group(0)
-            print(f"Received data ({data_raw})", flush=True)
-            imei = re.search(r"imei:(\d+),", data_raw).group(1)
-        except Exception as e:
-            print("Error parsing data: " + str(e), flush=True)
-            self.stream.close()
-            return
-        if not imei:
-            print("No imei", flush=True)
-            self.stream.close()
-            return
-        is_valid_imei = True
-        try:
-            validate_imei(imei)
-        except ValidationError:
-            is_valid_imei = False
-        if not is_valid_imei:
-            print("Invalid imei", flush=True)
-            self.stream.close()
-            return
-        print("Valid Imei", flush=True)
+    async def process_identification(self, imei):
+        if self.imei and imei != self.imei:
+            raise Exception("Cannot change IMEI")
+        validate_imei(imei)
         self.db_device = await get_device_by_imei(imei)
-        if not self.db_device:
-            print(f"Imei {imei} not registered  ({self.address})", flush=True)
-            self.stream.close()
-            return
-        self.imei = imei
-        print(f"{self.imei} is connected", flush=True)
-
-        await self._process_data(data_raw)
-
-        while await self._read_line():
-            pass
-
-    async def _process_data(self, data_raw):
-        data = data_raw.split(",")
         if not self.db_device.user_agent:
             self.db_device.user_agent = "Xexun ARM"
-        imei = re.search(r"imei:(\d+),", data_raw).group(1)
-        if imei != self.imei:
-            return False
-        self.logger.info(
-            f"XEXUN DATA, {self.aid}, {self.address}, {self.imei}: {','.join(data)}"
-        )
-        try:
-            tim = arrow.get(f"{data[9]} {data[1][:6]}", "DDMMYY HHmmss").int_timestamp
-            lat_minute = float(data[3])
-            lat = lat_minute // 100 + (lat_minute % 100) / 60
-            if data[4] == "S":
-                lat *= -1
-            lon_minute = float(data[5])
-            lon = lon_minute // 100 + (lon_minute % 100) / 60
-            if data[6] == "W":
-                lon *= -1
-        except Exception as e:
-            print(f"Could not parse GPS data {str(e)}", flush=True)
-        else:
+        if not self.db_device:
+            raise Exception("Imei not registered")
+        self.imei = imei
+        print(f"{self.imei} is connected")
 
-            loc_array = [(tim, lat, lon)]
-            if data[2] == "A":
-                await add_locations(self.db_device, loc_array)
-                print(f"{len(loc_array)} locations wrote to DB", flush=True)
+    async def start_listening(self):
+        print(f"Start listening from {self.address}")
+        while True:
+            imei = None
+            try:
+                data_raw = ""
+                while not data_raw:
+                    data_bin = await self.stream.read_bytes(255, partial=True)
+                    data_raw = data_bin[:-2].decode("ascii").strip()
+                    data_raw = re.search(r"G[PN]RMC,.+", data_raw).group(0)
+                print(f"Received data ({data_raw})", flush=True)
+                imei = re.search(r"imei:(\d+),", data_raw).group(1)
+            except Exception as e:
+                print(f"Error parsing data: {e}", flush=True)
+                self.stream.close()
+                return
 
-    async def _read_line(self):
-        try:
-            data_raw = ""
-            while not data_raw:
-                data_bin = await self.stream.read_bytes(255, partial=True)
-                data_raw = data_bin[:-2].decode("ascii").strip()
-                data_raw = re.search(r"G[PN]RMC,.+", data_raw).group(0)
-            print(f"Received data ({data_raw})")
-            await self._process_data(data_raw)
-        except Exception as e:
-            print(f"Error parsing data: {str(e)}")
-            self.stream.close()
-            return False
-        return True
+            try:
+                await self.process_identification(imei)
+            except Exception as e:
+                print(f"Could not identify device {e}", flush=True)
+                self.stream.close()
+                return
+            self.logger.info(
+                f"XEXUN DATA, {self.aid}, {self.address}, {self.imei}: {data_raw}"
+            )
+            try:
+                await self.process_data(data_raw)
+            except Exception as e:
+                print(f"Could not parse data {e}", flush=True)
+                self.stream.close()
+                return
+
+    async def process_data(self, data_raw):
+        data = data_raw.split(",")
+        if data[2] != "A":
+            return
+
+        tim = arrow.get(f"{data[9]} {data[1][:6]}", "DDMMYY HHmmss").int_timestamp
+        lat_minute = float(data[3])
+        lat = lat_minute // 100 + (lat_minute % 100) / 60
+        if data[4] == "S":
+            lat *= -1
+        lon_minute = float(data[5])
+        lon = lon_minute // 100 + (lon_minute % 100) / 60
+        if data[6] == "W":
+            lon *= -1
+
+        await add_locations(self.db_device, [(tim, lat, lon)])
+        print("1 locations wrote to DB", flush=True)
 
     def _on_close(self):
         print("Client quit", flush=True)
