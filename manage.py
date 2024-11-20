f1aa1d105e12ba6bde53c69326a5d8502453bfe8
Raphael Stefanini <raphael@rphlo.com>
2024-11-20 08:12:36 +0200

    Slim down manage to bare essential

--- manage.py
+++ manage.py
@@ -2,21 +2,8 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 if __name__ == "__main__":
-    argv = sys.argv
-    try:
-        command = argv[1]
-    except IndexError:
-        argv[1] = "help"
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "routechoices.settings")
-
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(argv)
+    execute_from_command_line(sys.argv)
