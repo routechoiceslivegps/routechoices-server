27bf2921bfc9e928a02cd2f916234c2186d530ab
Raphael Stefanini <raphael@rphlo.com>
2024-12-08 15:02:19 +0200

    Allow set club on event import

--- routechoices/core/bg_tasks.py
+++ routechoices/core/bg_tasks.py
@@ -3,6 +3,7 @@ import urllib.parse
 
 from background_task import background
 
+from routechoices.core.models import Club
 from routechoices.lib.other_gps_services.gpsseuranta import GpsSeurantaNet
 from routechoices.lib.other_gps_services.livelox import Livelox
 from routechoices.lib.other_gps_services.loggator import Loggator
@@ -21,74 +22,88 @@ class MapImportError(Exception):
 
 
 @background(schedule=0)
-def import_single_event_from_gps_seuranta(event_id):
+def import_single_event_from_gps_seuranta(event_id, club=None):
     event_id = event_id.strip()
     if match := re.match(
         r"https?://((gps|www)\.)?tulospalvelu\.fi/gps/(?P<uid>[^/]+)/?", event_id
     ):
         event_id = match.group("uid")
     solution = GpsSeurantaNet()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_virekunnas(event_id):
+def import_single_event_from_virekunnas(event_id, club=None):
     event_id = event_id.strip()
     if match := re.match(r"https?://gps\.virekunnas\.fi/(?P<uid>[^/]+)/?", event_id):
         event_id = match.group("uid")
     solution = GpsVirekunnasFi()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_loggator(event_id):
+def import_single_event_from_loggator(event_id, club=None):
     event_id = event_id.strip()
     if match := re.match(
         r"https?://(events\.)?loggator\.com/(?P<uid>[^/]+)/?", event_id
     ):
         event_id = match.group("uid")
     solution = Loggator()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_tractrac(event_id):
+def import_single_event_from_tractrac(event_id, club=None):
     prefix = "https://live.tractrac.com/viewer/index.html?target="
     if event_id.startswith(prefix):
         event_id = event_id[len(prefix) :]
     solution = Tractrac()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_otracker(event_id):
+def import_single_event_from_otracker(event_id, club=None):
     prefix = "https://otracker.lt/events/"
     if event_id.startswith(prefix):
         event_id = event_id[len(prefix) :]
     solution = OTracker()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_sportrec(event_id):
+def import_single_event_from_sportrec(event_id, club=None):
     prefix = "https://sportrec.eu/gps/"
     if event_id.startswith(prefix):
         event_id = event_id[len(prefix) :]
     solution = SportRec()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
 
 
 @background(schedule=0)
-def import_single_event_from_livelox(event_id):
+def import_single_event_from_livelox(event_id, club=None):
     prefix = "https://www.livelox.com/Viewer/"
     if event_id.startswith(prefix):
         event_id = urllib.parse.urlparse(event_id).query
     solution = Livelox()
+    if club:
+        solution.club = Club.objects.get(slug=club)
     event = solution.import_event(event_id)
     return event
--- routechoices/core/management/commands/import_event.py
+++ routechoices/core/management/commands/import_event.py
@@ -9,6 +9,7 @@ from routechoices.core.bg_tasks import (
     import_single_event_from_tractrac,
     import_single_event_from_virekunnas,
 )
+from routechoices.core.models import Club
 from routechoices.lib.other_gps_services.commons import EventImportError
 
 
@@ -25,47 +26,58 @@ class Command(BaseCommand):
             help="Import from gpsseuranta",
         )
         gpsseuranta_parser.set_defaults(method=self.gpsseuranta)
+        gpsseuranta_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         virekunnas_parser = subparsers.add_parser(
             "virekunnas",
             help="Import from virekunnas",
         )
         virekunnas_parser.set_defaults(method=self.virekunnas)
+        virekunnas_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         livelox_parser = subparsers.add_parser(
             "livelox",
             help="Import from livelox",
         )
         livelox_parser.set_defaults(method=self.livelox)
+        livelox_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         loggator_parser = subparsers.add_parser(
             "loggator",
             help="Import from loggator",
         )
         loggator_parser.set_defaults(method=self.loggator)
+        loggator_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         otracker_parser = subparsers.add_parser(
             "otracker",
             help="Import from otracker",
         )
         otracker_parser.set_defaults(method=self.otracker)
+        otracker_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         sportrec_parser = subparsers.add_parser(
             "sportrec",
             help="Import from sportrec",
         )
         sportrec_parser.set_defaults(method=self.sportrec)
+        sportrec_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
         tractrac_parser = subparsers.add_parser(
             "tractrac",
             help="Import from tractrac",
         )
         tractrac_parser.set_defaults(method=self.tractrac)
+        tractrac_parser.add_argument(dest="event_ids", nargs="+", type=str)
 
-        parser.add_argument("event_ids", nargs="+", type=str)
-        parser.add_argument("--task", action="store_true", default=False)
+        parser.add_argument("-t", "--task", action="store_true", default=False)
+        parser.add_argument("-c", "--club", type=str)
 
     def handle(self, *args, method, **options):
+        club = Club.objects.filter(slug=options["club"]).first()
+        if options["club"] and not club:
+            self.stderr.write(f"Could not find club {options['club']}")
+            return
         method(*args, **options)
 
     def gpsseuranta(self, *args, **options):
@@ -73,9 +85,13 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_gps_seuranta(event_id)
+                    import_single_event_from_gps_seuranta(
+                        event_id, club=options["club"]
+                    )
                 else:
-                    import_single_event_from_gps_seuranta.now(event_id)
+                    import_single_event_from_gps_seuranta.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
@@ -85,9 +101,11 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_virekunnas(event_id)
+                    import_single_event_from_virekunnas(event_id, club=options["club"])
                 else:
-                    import_single_event_from_virekunnas.now(event_id)
+                    import_single_event_from_virekunnas.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
@@ -97,9 +115,9 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_livelox(event_id)
+                    import_single_event_from_livelox(event_id, club=options["club"])
                 else:
-                    import_single_event_from_livelox.now(event_id)
+                    import_single_event_from_livelox.now(event_id, club=options["club"])
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
@@ -109,9 +127,11 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_loggator(event_id)
+                    import_single_event_from_loggator(event_id, club=options["club"])
                 else:
-                    import_single_event_from_loggator.now(event_id)
+                    import_single_event_from_loggator.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
@@ -121,9 +141,11 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_otracker(event_id)
+                    import_single_event_from_otracker(event_id, club=options["club"])
                 else:
-                    import_single_event_from_otracker.now(event_id)
+                    import_single_event_from_otracker.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
@@ -133,9 +155,11 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_sportrec(event_id)
+                    import_single_event_from_sportrec(event_id, club=options["club"])
                 else:
-                    import_single_event_from_sportrec.now(event_id)
+                    import_single_event_from_sportrec.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError as e:
                 self.stderr.write(f"Could not import event {event_id}: {str(e)}")
                 continue
@@ -145,9 +169,11 @@ class Command(BaseCommand):
             try:
                 self.stdout.write(f"Importing event {event_id}")
                 if options["task"]:
-                    import_single_event_from_tractrac(event_id)
+                    import_single_event_from_tractrac(event_id, club=options["club"])
                 else:
-                    import_single_event_from_tractrac.now(event_id)
+                    import_single_event_from_tractrac.now(
+                        event_id, club=options["club"]
+                    )
             except EventImportError:
                 self.stderr.write(f"Could not import event {event_id}")
                 continue
