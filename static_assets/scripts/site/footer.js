20747387f4a076a4d87f6e4e5e6d8ebbf534bbfd
Raphael Stefanini <raphael@rphlo.com>
2024-12-18 09:40:49 +0200

    Add status page indicator

---
+++ static_assets/scripts/site/footer.js
@@ -0,0 +1,11 @@
+(async function updateStatus() {
+	await fetch("https://j9njnl3qx7d8.statuspage.io/api/v2/status.json")
+		.then((r) => r.json())
+		.then((data) => {
+			const className = `status-indicator-${data.status.indicator}`;
+			const el = document.getElementById("status-indicator");
+			el.classList.remove("status-indicator-default");
+			el.classList.add(className);
+			el.parentElement().setAttribute("title", data.status.description);
+		});
+})();
