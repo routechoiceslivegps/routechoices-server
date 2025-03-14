80659db8504a7c4e2e2e76ecd3c646b3f3aa6f35
Raphael Stefanini <raphael@rphlo.com>
2025-03-14 08:28:47 +0200

    wip: missing images

---
+++ static_assets/vendor/gpsseuranta/js/load3.js
@@ -0,0 +1,154 @@
+var eventPath = new String(document.location);
+var eventHost = window.location.host;
+var eventId = "";
+var testEventPath = "";
+pathArray = eventPath.split("?");
+images = new Array()
+var cdnpath = "//dln0s0v92ab5h.cloudfront.net/gps/client/ver20181107/";
+var isgpxseuranta = false;
+var hideallmenus = false;
+var langu = 0;
+
+var sessionid="";
+var issessioned = false;
+var sessioneventPath = new String(document.location);
+var sessionpathArray = sessioneventPath.split("?");
+if(sessionpathArray.length > 1) {
+	var sessionparams =  sessionpathArray[1];
+	var sessionpathArray2 = sessionparams.split("&");
+	for(sii=0;sii<sessionpathArray2.length;sii++) {
+		if(sessionpathArray2[sii].indexOf("GpsSeurantaSessionID=")==0) {
+			sessionid =  sessionpathArray2[sii].substring(21);
+			issessioned = true;
+			break;
+		}
+	}
+}
+window.addEventListener("message", function(event) {
+	kasky = event.data;
+	if(kasky.startsWith("center:")) {
+		trevent.centercompetitorid(mapHeight, kasky.substring(7));
+	}
+});
+var forceMapSystem =0;
+if(pathArray.length > 1) {
+   var params =  pathArray[1];
+	if(params.indexOf("map=MML2")>-1) {
+	forceMapSystem =5;
+	eventPath = eventPath.replace("&map=MML2","");
+	eventPath = eventPath.replace("map=MML2","");
+	pathArray = eventPath.split("?");
+   }
+   else if (params.indexOf("map=MML3")>-1) {
+	forceMapSystem =6;
+	eventPath = eventPath.replace("&map=MML3","");
+	eventPath = eventPath.replace("map=MML3","");
+	pathArray = eventPath.split("?");
+  }
+
+
+   else if(params.indexOf("map=MML")>-1) {
+	forceMapSystem =5;
+	eventPath = eventPath.replace("&map=MML","");
+	eventPath = eventPath.replace("map=MML","");
+	pathArray = eventPath.split("?");
+   }
+   else if (params.indexOf("map=NOR")>-1) {
+	forceMapSystem =2;
+	eventPath = eventPath.replace("&map=NOR","");
+	eventPath = eventPath.replace("map=NOR","");
+	pathArray = eventPath.split("?");
+   } else if (params.indexOf("map=OSM")>-1) {
+	forceMapSystem =3;
+	eventPath = eventPath.replace("&map=OSM","");
+	eventPath = eventPath.replace("map=OSM","");
+	pathArray = eventPath.split("?");
+   } else if (params.indexOf("map=OTM")>-1) {
+	forceMapSystem =7;
+	eventPath = eventPath.replace("&map=OTM","");
+	eventPath = eventPath.replace("map=OTM","");
+	pathArray = eventPath.split("?");
+   } else if (params.indexOf("map=SWE")>-1) {
+	forceMapSystem =8;
+	eventPath = eventPath.replace("&map=SWE","");
+	eventPath = eventPath.replace("map=SWE","");
+	pathArray = eventPath.split("?");
+   } else if (params.indexOf("map=OTH")>-1) {
+	forceMapSystem =9;
+	eventPath = eventPath.replace("&map=OTH","");
+	eventPath = eventPath.replace("map=OTH","");
+	pathArray = eventPath.split("?");
+   }
+
+  if(params.indexOf("hidemenus=1")>-1) {
+		 hideallmenus = true;
+		 eventPath = eventPath.replace("&hidemenus=1","");
+		 eventPath = eventPath.replace("hidemenus=1","");
+		 pathArray = eventPath.split("?");
+  }
+
+}
+
+
+
+var appMode = "";
+var compColArray = [];
+
+var timeeventhost = "//gps.virekunnas.fi";
+
+// http://stackoverflow.com/questions/19012135/ios-7-ipad-safari-landscape-innerheight-outerheight-layout-issue
+if (navigator.userAgent.match(/iPad;.*CPU.*OS 7_\d/i) && !window.navigator.standalone) {
+    $('html').addClass('ios7-fix');
+}
+
+// lataa kuvat ennakkoon
+function preload() {
+	for (i = 0; i < preload.arguments.length; i++) {
+		images[i] = new Image()
+		images[i].src = preload.arguments[i]
+	}
+}
+preload(
+	cdnpath+"images/arrowRi.png", cdnpath+"images/arrowLe.png",
+	cdnpath+"images/arrowDown.png", cdnpath+"images/arrowDown2.png",
+	cdnpath+"images/arrowUp.png", cdnpath+"images/arrowUp2.png",
+	cdnpath+"images/btnPlay.png", cdnpath+"images/btnPaused.png"
+)
+delete images;
+
+//var eventId = pathArray[pathArray.length - 2]; // haetaan taulukon viimeinen ei tyhjä alkio joka sisältää ID:n
+//if(pathArray.length > 1) {
+	//eventId = pathArray[0];
+	//var last = eventId.length;
+
+	//if(eventId.charAt(last) == "/") eventId = eventId.substr(3, last-1);
+	//else eventId = eventId.substr(3, last);
+
+	var pathparts = eventPath.split('/');
+	var last = pathparts.length;
+	eventId = pathparts[last-2];
+	var eventpathalku = eventPath.substr(0, eventPath.indexOf(eventId));
+
+	testEventPath = eventpathalku  + eventId+"/";
+	timeeventhost = "//"+pathparts[2];
+	console.log("teh: "+timeeventhost);
+delete pathArray;
+
+console.log("PATH " + eventPath + " HOST " + eventHost + " ID " + eventId);
+
+// hae oikea CSS filu ennen index sivun body tagia
+// Keksien hallintaa ja sovellustilan asettaminen käynnistäessä ohjelmaa
+if (document.cookie.search(/(^|;)gpsAppMode=/) < 0) {
+	if (/Android|webOS|iPhone|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		document.cookie = "gpsAppMode=mobile";
+	} else {
+		document.cookie = "gpsAppMode=desktop";
+	}
+}
+// hae keksi
+function getCookie(name) {
+	var re = new RegExp(name + "=([^;]+)");
+	var value = re.exec(document.cookie);
+	return (value != null) ? unescape(value[1]) : null;
+}
+var appMode = getCookie("gpsAppMode");
---
+++ static_assets/vendor/gpsseuranta/js/strings.js
@@ -0,0 +1,130 @@
+var Strings_Center = ['Center','Center',decodeURIComponent('%E4%B8%AD%E5%BF%83')];
+var Strings_Autocenter_ON = ['Autocenter ON','Autocenter ON',decodeURIComponent('%E8%87%AA%E5%8A%A8%E5%B1%85%E4%B8%AD %E5%BC%80%E5%90%AF')];
+var Strings_Autocenter_OFF = ['Autocenter OFF','Autocenter OFF',decodeURIComponent('%E8%87%AA%E5%8A%A8%E5%B1%85%E4%B8%AD %E5%85%B3%E9%97%AD')];
+var Strings_Highlight_ON = ['Highlight ON','Highlight ON',decodeURIComponent('%E7%AA%81%E5%87%BA%E6%98%BE%E7%A4%BA %E5%BC%80%E5%90%AF')];
+var Strings_Highlight_OFF = ['Highlight OFF','Highlight OFF',decodeURIComponent('%E7%AA%81%E5%87%BA%E6%98%BE%E7%A4%BA %E5%85%B3%E9%97%AD')];
+var Strings_Full_route_ON = ['Full route ON','Full route ON',decodeURIComponent('%E5%85%A8%E7%A8%8B%E8%B7%AF%E7%BA%BF %E5%BC%80%E5%90%AF')];
+var Strings_Full_route_OFF = ['Full route OFF','Full route OFF',decodeURIComponent('%E5%85%A8%E7%A8%8B%E8%B7%AF%E7%BA%BF %E5%85%B3%E9%97%AD')];
+var Strings_Check_above = ['Check above','Check above',decodeURIComponent('%E6%9F%A5%E7%9C%8B%E6%AD%A4%E4%B8%8A%E9%9D%A2%E7%9A%84')];
+var Strings_Choose_color = ['Choose color:','Choose color:',decodeURIComponent('%E9%80%89%E6%8B%A9%E9%A2%9C%E8%89%B2%EF%BC%9A')];
+var Strings_OK = ['OK','OK',decodeURIComponent('OK')];
+var Strings_Competitors = ['Competitors...','Competitors...',decodeURIComponent('%E6%AF%94%E8%B5%9B%E9%80%89%E6%89%8B...')];
+var Strings_App_Mode = ['App Mode','App Mode',decodeURIComponent('%E5%BA%94%E7%94%A8%E6%A8%A1%E5%BC%8F')];
+var Strings_Desktop = ['Desktop','Desktop',decodeURIComponent('%E6%A1%8C%E9%9D%A2')];
+var Strings_Mobile = ['Mobile','Mobile',decodeURIComponent('%E7%A7%BB%E5%8A%A8%E7%AB%AF')];
+var Strings_Tail = ['Tail','Tail',decodeURIComponent('%E8%BD%A8%E8%BF%B9')];
+var Strings_Route = ['Route','Route',decodeURIComponent('%E8%B7%AF%E7%BA%BF')];
+
+
+var Strings_loading_data = ['loading data...','loading data...',decodeURIComponent('%E5%8A%A0%E8%BD%BD%E6%95%B0%E6%8D%AE%E4%B8%AD%E2%80%A6')];
+var Strings_PRE = ['PRE','PRE',decodeURIComponent('%E5%B0%B1%E7%BB%AA')];
+var Strings_LIVE = ['LIVE','LIVE',decodeURIComponent('%E5%AE%9E%E6%97%B6')];
+var Strings_OK = ['OK','OK',decodeURIComponent('%E6%AD%A3%E5%B8%B8')];
+var Strings_POST = ['POST','POST',decodeURIComponent('%E7%BB%93%E6%9D%9F')];
+var Strings_BREAK = ['BREAK','BREAK',decodeURIComponent('%E5%87%86%E5%A4%87')];
+var Strings_NA = ['N/A','N/A',decodeURIComponent('%E6%97%A0%E6%95%B0%E6%8D%AE')];
+var Strings_Len = ['Len','Len',decodeURIComponent('%E5%B0%BE%E9%95%BF')];
+var Strings_Predefsplitpoints = ['Pre-def splitpoints','Pre-def splitpoints',decodeURIComponent('%E9%A2%84%E5%AE%9A%E4%B9%89%E5%88%86%E5%89%B2%E7%82%B9')];
+
+
+var Strings_draw_tail_end = ['draw tail end','draw tail end',decodeURIComponent('%E7%BB%98%E5%88%B6%E8%BD%A8%E8%BF%B9%E7%BB%93%E5%B0%BE')];
+var Strings_Marker_line_map = ['Marker, line & map','Marker, line & map',decodeURIComponent('%E6%A0%87%E8%AE%B0%E3%80%81%E7%BA%BF%E6%9D%A1%E5%92%8C%E5%9C%B0%E5%9B%BE')];
+var Strings_Marker_size = ['Marker size:','Marker size:',decodeURIComponent('%E6%A0%87%E8%AE%B0%E5%A4%A7%E5%B0%8F%EF%BC%9A')];
+var Strings_px = ['px','px',decodeURIComponent('%E5%83%8F%E7%B4%A0')];
+var Strings_Letters = ['Letters:','Letters:',decodeURIComponent('%E5%AD%97%E6%AF%8D%EF%BC%9A')];
+var Strings_Line_width = ['Line width:','Line width:',decodeURIComponent('%E7%BA%BF%E6%9D%A1%E5%AE%BD%E5%BA%A6')];
+var Strings_Line_opacity = ['Line opacity:','Line opacity:',decodeURIComponent('%E7%BA%BF%E6%9D%A1%E9%80%8F%E6%98%8E%E5%BA%A6')];
+var Strings_Map_opacity = ['Map opacity:','Map opacity:',decodeURIComponent('%E5%9C%B0%E5%9B%BE%E9%80%8F%E6%98%8E%E5%BA%A6')];
+var Strings_Competitor_info = ['Competitor info','Competitor info',decodeURIComponent('%E6%AF%94%E8%B5%9B%E9%80%89%E6%89%8B%E4%BF%A1%E6%81%AF')];
+var Strings_Show = ['Show','Show',decodeURIComponent('%E6%98%BE%E7%A4%BA')];
+var Strings_Hide = ['Hide','Hide',decodeURIComponent('%E9%9A%90%E8%97%8F')];
+var Strings_kmh = ['km/h','km/h',decodeURIComponent('km%2Fh')];
+var Strings_minkm = ['min/km','min/km',decodeURIComponent('min%2Fkm')];
+var Strings_knot = ['knot','knot',decodeURIComponent('knot')];
+var Strings_Offset = ['Offset','Offset',decodeURIComponent('%E5%81%8F%E7%A7%BB%E9%87%8F')];
+var Strings_Reset_all_offsets = ['Reset all offsets','Reset all offsets',decodeURIComponent('%E9%87%8D%E7%BD%AE%E6%89%80%E6%9C%89%E5%81%8F%E7%A7%BB')];
+
+var Strings_Select_competitor_first = ['Select competitor first','Select competitor first',decodeURIComponent('%E9%A6%96%E5%85%88%E9%80%89%E6%8B%A9%E6%AF%94%E8%B5%9B%E9%80%89%E6%89%8B')];
+
+
+var Strings_Debug = ['Debug','Debug',decodeURIComponent('%E8%B0%83%E8%AF%95')];
+var Strings_REAL = ['REAL','REAL',decodeURIComponent('%E5%AE%9E%E6%97%B6')];
+var Strings_SYNC = ['SYNC','SYNC',decodeURIComponent('%E5%90%8C%E6%AD%A5')];
+var Strings_LIVE = ['LIVE','LIVE',decodeURIComponent('%E7%9B%B4%E6%92%AD')];
+var Strings_REPLAY = ['REPLAY','REPLAY',decodeURIComponent('%E9%87%8D%E6%92%AD')];
+
+var Strings_LIST = ['LIST','LIST',decodeURIComponent('%E5%88%97%E8%A1%A8')];
+var Strings_OPTIONS = ['OPTIONS','OPTIONS',decodeURIComponent('%E9%80%89%E9%A1%B9')];
+
+var Strings_SPLITS = ['SPLITS','SPLITS',decodeURIComponent('%E6%8B%86%E5%88%86')];
+var Strings_Settings = ['Settings','Settings',decodeURIComponent('%E8%AE%BE%E7%BD%AE')];
+var Strings_Splits = ['Splits','Splits',decodeURIComponent('%E6%8B%86%E5%88%86')];
+var Strings_Sector = ['Sector','Sector',decodeURIComponent('%E5%8C%BA%E5%9F%9F')];
+var Strings_Diff = ['Diff','Diff',decodeURIComponent('%E5%B7%AE%E5%BC%82')];
+var Strings_Right_click_long_tap_map = ['Right-click/long tap map to make splitpoints/lines','Right-click/long tap map to make splitpoints/lines',decodeURIComponent('%E5%8F%B3%E9%94%AE%E5%8D%95%E5%87%BB%2F%E9%95%BF%E6%8C%89%E5%9C%B0%E5%9B%BE%E4%BB%A5%E5%88%9B%E5%BB%BA%E5%88%86%E5%89%B2%E7%82%B9%2F%E7%BA%BF')];
+var Strings_Lines_points_on_the_map = ['Lines/points on the map','Lines/points on the map',decodeURIComponent('%E5%9C%B0%E5%9B%BE%E4%B8%8A%E7%9A%84%E7%BA%BF%2F%E7%82%B9')];
+var Strings_Time_to_use = ['Time to use','Time to use',decodeURIComponent('%E4%BD%BF%E7%94%A8%E6%97%B6%E9%97%B4')];
+var Strings_Race = ['Race','Race',decodeURIComponent('%E6%AF%94%E8%B5%9B')];
+var Strings_Real = ['Real','Real',decodeURIComponent('%E5%AE%9E%E6%97%B6')];
+var Strings_Diff = ['Diff.','Diff.',decodeURIComponent('%E5%B7%AE%E8%B7%9D')];
+
+var Strings_Points = ['Points','Points',decodeURIComponent('%E7%82%B9')];
+
+var Strings_Times = ['Times','Times',decodeURIComponent('%E6%97%B6%E9%97%B4')];
+var Strings_Names = ['Names','Names',decodeURIComponent('%E5%90%8D%E5%AD%97')];
+var Strings_Letters = ['Letters','Letters',decodeURIComponent('%E5%AD%97%E6%AF%8D')];
+var Strings_All = ['All','All',decodeURIComponent('%E6%89%80%E6%9C%89')];
+var Strings_Checked = ['Checked','Checked',decodeURIComponent('%E5%B7%B2%E6%A3%80%E6%9F%A5')];
+var Strings_Font_size = ['Font size','Font size',decodeURIComponent('%E5%AD%97%E4%BD%93%E5%A4%A7%E5%B0%8F')];
+var Strings_Ignore_times_before = ['Ignore times before','Ignore times before',decodeURIComponent('%E5%BF%BD%E7%95%A5%E4%B9%8B%E5%89%8D%E7%9A%84%E6%97%B6%E9%97%B4')];
+var Strings_Real_time = ['Real time','Real time',decodeURIComponent('%E5%AE%9E%E6%97%B6%E6%97%B6%E9%97%B4')];
+var Strings_not_set = ['(not set)','(not set)',decodeURIComponent('%EF%BC%88%E6%B2%A1%E6%9C%89%E8%AE%BE%E7%BD%AE%EF%BC%89')];
+var Strings_Race_time = ['Race time','Race time',decodeURIComponent('%E6%AF%94%E8%B5%9B%E6%97%B6%E9%97%B4')];
+var Strings_Delete_point_line = ['Delete point/line','Delete point/line',decodeURIComponent('%E5%88%A0%E9%99%A4%E7%82%B9%2F%E7%BA%BF')];
+var Strings_From = ['From:','From:',decodeURIComponent('%E4%BB%8E%EF%BC%9A')];
+var Strings_start = ['start','start',decodeURIComponent('%E5%BC%80%E5%A7%8B')];
+var Strings_Lap = ['Lap:','Lap:',decodeURIComponent('%E5%9C%88%E6%95%B0%EF%BC%9A')];
+var Strings_To = ['To:','To:',decodeURIComponent('%E5%88%B0%EF%BC%9A')];
+var Strings_Calculate = ['Calculate','Calculate',decodeURIComponent('%E8%AE%A1%E7%AE%97')];
+var Strings_Routes = ['Routes','Routes',decodeURIComponent('%E8%B7%AF%E7%BA%BF')];
+var Strings_color_by_time = ['color by time','color by time',decodeURIComponent('%E6%8C%89%E6%97%B6%E9%97%B4%E7%9A%84%E9%A2%9C%E8%89%B2')];
+var Strings_Time_difference_between_2_competitors = ['Time difference between 2 competitors','Time difference between 2 competitors',decodeURIComponent('2%E4%B8%AA%E6%AF%94%E8%B5%9B%E9%80%89%E6%89%8B%E4%B9%8B%E9%97%B4%E7%9A%84%E6%97%B6%E5%B7%AE')];
+var Strings_First = ['First','First',decodeURIComponent('%E7%AC%AC%E4%B8%80')];
+var Strings_Behind = ['Behind','Behind',decodeURIComponent('%E8%90%BD%E5%90%8E')];
+var Strings_Min_max_differences = ['Min-max differences (minutes)','Min-max differences (minutes)',decodeURIComponent('%E6%9C%80%E5%B0%8F -%E6%9C%80%E5%A4%A7%E5%B7%AE%E5%BC%82%EF%BC%88%E5%88%86%E9%92%9F%EF%BC%89')];
+var Strings_Calculate_time_difference = ['Calculate time difference between two competitors if their routes are within 50 meters.','Calculate time difference between two competitors if their routes are within 50 meters.',decodeURIComponent('%E5%A6%82%E6%9E%9C%E4%B8%A4%E4%B8%AA%E6%AF%94%E8%B5%9B%E9%80%89%E6%89%8B%E7%9A%84%E8%B7%AF%E7%BA%BF%E5%9C%A850%E7%B1%B3%E4%BB%A5%E5%86%85%EF%BC%8C%E5%88%99%E8%AE%A1%E7%AE%97%E4%BB%96%E4%BB%AC%E4%B9%8B%E9%97%B4%E7%9A%84%E6%97%B6%E5%B7%AE%E3%80%82')];
+var Strings_Difference_must_be_between_min_max = ['Difference must be between min-max.','Difference must be between min-max.',decodeURIComponent('%E6%9C%80%E5%B0%8F-%E6%9C%80%E5%A4%A7%E4%B9%8B%E9%97%B4%E5%BF%85%E9%A1%BB%E6%9C%89%E5%B7%AE%E5%BC%82%E3%80%82')];
+var Strings_Doesnt = ['Doesn\'t work well on controls/other sharp turns.','Doesn\'t work well on controls/other sharp turns.',decodeURIComponent('%E5%9C%A8%E6%A3%80%E6%9F%A5%E7%82%B9%2F%E5%85%B6%E5%AE%83%E6%80%A5%E8%BD%AC%E5%BC%AF%E4%B8%8A%E6%95%88%E6%9E%9C%E4%B8%8D%E4%BD%B3%E3%80%82')];
+
+
+
+
+var Strings_Rogaining_points = ['Rogaining points (experimental)','Rogaining points (experimental)',decodeURIComponent('%E7%A7%AF%E5%88%86%E7%82%B9%EF%BC%88%E5%AE%9E%E9%AA%8C%E6%80%A7%EF%BC%89')];
+var Strings_Show_on_map = ['Show on map','Show on map',decodeURIComponent('%E5%9C%A8%E5%9C%B0%E5%9B%BE%E4%B8%8A%E6%98%BE%E7%A4%BA')];
+var Strings_Edit_settings = ['Edit settings','Edit settings',decodeURIComponent('%E7%BC%96%E8%BE%91%E8%AE%BE%E7%BD%AE')];
+var Strings_New_split_line = ['New split line','New split line',decodeURIComponent('%E6%96%B0%E7%9A%84%E5%88%86%E5%89%B2%E7%BA%BF')];
+var Strings_New_split_point = ['New split point','New split point',decodeURIComponent('%E6%96%B0%E7%9A%84%E5%88%86%E5%89%B2%E7%82%B9')];
+
+
+var Strings_realtime = ['real time','real time',decodeURIComponent('%E5%AE%9E%E6%97%B6%E6%97%B6%E9%97%B4')];
+var Strings_racetime = ['race time','race time',decodeURIComponent('%E6%AF%94%E8%B5%9B%E6%97%B6%E9%97%B4')];
+var Strings_Add_splitpointslines = ['Add splitpoints/lines','Add splitpoints/lines',decodeURIComponent('%E6%B7%BB%E5%8A%A0%E5%88%86%E5%89%B2%E7%82%B9%2F%E7%BA%BF')];
+var Strings_New_split_pointline = ['New split point/line','New split point/line',decodeURIComponent('%E6%96%B0%E7%9A%84%E5%88%86%E5%89%B2%E7%82%B9%2F%E7%BA%BF')];
+
+
+
+var Strings_Name = ['Name','Name',decodeURIComponent('%E5%90%8D%E7%A7%B0')];
+var Strings_After = ['After','After',decodeURIComponent('%E4%B9%8B%E5%90%8E')];
+var Strings_first = ['(first)','(first)',decodeURIComponent('%EF%BC%88%E7%AC%AC%E4%B8%80%EF%BC%89')];
+var Strings_Radius = ['Radius','Radius',decodeURIComponent('%E5%8D%8A%E5%BE%84')];
+var Strings_m = ['m','m',decodeURIComponent('m')];
+var Strings_Cancel = ['Cancel','Cancel',decodeURIComponent('%E5%8F%96%E6%B6%88')];
+var Strings_Add_splitline = ['Add splitline','Add splitline',decodeURIComponent('%E6%B7%BB%E5%8A%A0%E5%88%86%E5%89%B2%E7%BA%BF')];
+
+var Strings_Add_splitpoint = ['Add splitpoint','Add splitpoint',decodeURIComponent('%E6%B7%BB%E5%8A%A0%E5%88%86%E5%89%B2%E7%82%B9')];
+
+var Strings_Mass_start_here = ['Mass-start here','Mass-start here',decodeURIComponent('%E4%BB%8E%E8%BF%99%E9%87%8C%E5%BC%80%E5%A7%8B')];
+var Strings_Reset_all_offsets = ['Reset all offsets','Reset all offsets',decodeURIComponent('%E9%87%8D%E7%BD%AE%E6%89%80%E6%9C%89%E5%81%8F%E7%A7%BB')];
+
+var Strings_AppMode = ['App Mode','moodi',decodeURIComponent('&#32735&#26790&#38597')];
+var Strings_Real = ['REAL', 'reaali', decodeURIComponent('&#32735&#26790&#38597')];
