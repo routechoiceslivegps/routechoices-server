fe7abe442f0d95bac2d5a889d69ef2cb14a808bb
Raphael Stefanini <raphael@rphlo.com>
2025-04-16 14:08:10 +0300

    Allow multithreaded TCP server

--- bin/run_tornado.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-/apps/routechoices-server/env/bin/python /apps/routechoices-server/manage.py run_tcp_server --tmt250-port=12000 --mictrack-port=12001 --queclink-port=12002 --tracktape-port=12003 --xexun-port=12004 --gt06-port=12005
