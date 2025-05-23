28a4194e7ec914df80073646f2a9b9fe102e50ee
Raphael Stefanini <raphael@rphlo.com>
2025-05-23 14:18:06 +0300

    Add club clean up daily command

--- routechoices/core/management/commands/clean_clubs.py
+++ routechoices/core/management/commands/clean_clubs.py
@@ -16,18 +16,17 @@ class Command(BaseCommand):
     help = "Remove club without activity"
 
     def send_warning(self, days, club):
-        print(f"- {club.name}")
-        print(f"  {days} warning")
+        print(f"- {club.name} -> {days} warning")
         admins = club.admins.all().values_list("id", flat=True)
         if not admins:
+            print("  No admins to send emails to\n")
             return
         to_emails = list(
             EmailAddress.objects.filter(
                 user_id__in=admins, verified=True, primary=True
             ).values_list("email", flat=True)
         )
-        print(f"  Sending Email to {", ".join(to_emails)}")
-        print()
+        print(f"  Sending Email to {", ".join(to_emails)}\n")
         msg = EmailMessage(
             f'[Routechoices.com] ACTION REQUIRED, Your club "{club.name}" may get deleted in {days}',
             f"""Hi,
@@ -46,7 +45,29 @@ https://www.routechoices.com/login
         msg.send()
 
     def delete_club(self, club):
-        print(f"DELETING {club.name}")
+        print(f"- {club.name} -> DELETING NOW")
+        admins = club.admins.all().values_list("id", flat=True)
+        if not admins:
+            print("  No admins to send emails to\n")
+        else:
+            to_emails = list(
+                EmailAddress.objects.filter(
+                    user_id__in=admins, verified=True, primary=True
+                ).values_list("email", flat=True)
+            )
+            print(f"  Sending Email to {", ".join(to_emails)}\n")
+            msg = EmailMessage(
+                f'[Routechoices.com] Your club "{club.name}" has been deleted',
+                f"""Hi,
+
+We've noticed you haven't signed in to your Routechoices.com account in a while. In fact it's been more than a year! We routinely remove inactive accounts to ensure we're not storing any data you don't want us to.
+
+This email is to let you know that we have now deleted your inactive Routechoices.com club "{club.name}", along with any data in your club (such as your maps and events).
+""",
+                settings.DEFAULT_FROM_EMAIL,
+                list(to_emails),
+            )
+            msg.send()
         club.delete()
 
     def handle(self, *args, **options):
@@ -55,6 +76,11 @@ https://www.routechoices.com/login
         tomorrow = (nyt + relativedelta(days=1)).date()
         seven_days_from_now = (nyt + relativedelta(days=7)).date()
         thirty_days_from_now = (nyt + relativedelta(days=30)).date()
+
+        thirthy_days_warning_count = 0
+        seven_days_warning_count = 0
+        twenty_four_hours_warning_count = 0
+        deleted_count = 0
 
         clubs = (
             Club.objects.annotate(last_login=Max("admins__last_login"))
@@ -73,9 +99,24 @@ https://www.routechoices.com/login
             ).date()
             if due_deletion == today:
                 self.delete_club(club)
+                deleted_count += 1
             elif due_deletion == tomorrow:
                 self.send_warning("24 hours", club)
+                twenty_four_hours_warning_count += 1
             elif due_deletion == seven_days_from_now:
                 self.send_warning("7 days", club)
+                seven_days_warning_count += 1
             elif due_deletion == thirty_days_from_now:
                 self.send_warning("30 days", club)
+                thirthy_days_warning_count += 1
+
+        self.stdout.write(
+            f"30 days to deletion warning sent: {thirthy_days_warning_count}"
+        )
+        self.stdout.write(
+            f"7 days to deletion warning sent: {seven_days_warning_count}"
+        )
+        self.stdout.write(
+            f"24 hours to deletion warning sent: {twenty_four_hours_warning_count}"
+        )
+        self.stdout.write(f"Clubs deleted: {deleted_count}")
