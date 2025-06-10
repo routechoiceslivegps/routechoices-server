0591c2a109b79ea9532a044fb96ae6f44871bd7d
Raphael Stefanini <raphael@rphlo.com>
2025-06-10 07:47:17 +0300

    Fix livelox float range

--- routechoices/lib/other_gps_services/livelox.py
+++ routechoices/lib/other_gps_services/livelox.py
@@ -665,8 +665,6 @@ class LiveloxBase64Reader:
         t = 0
         lat = 0
         lng = 0
-        if isinstance(k, float):
-            k = int(k)
         for _ in range(k):
             t += self.read_value()
             lat += self.read_value()
