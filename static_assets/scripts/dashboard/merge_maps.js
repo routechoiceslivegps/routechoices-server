4066d8c8293069fb79c040f1c5b2e7811ffb0704
Raphael Stefanini <raphael@rphlo.com>
2025-06-21 13:03:14 +0300

    Add some JS

--- static_assets/scripts/dashboard/merge_maps.js
+++ static_assets/scripts/dashboard/merge_maps.js
@@ -1,11 +1,12 @@
 (() => {
-	u("#id_base").on("change", (e) => {
-		u("#id_addend option").each((opt) => {
+	document.getElementById("id_base").addEventListener("change", (e) => {
+		const options = document.querySelectorAll("#id_addend option");
+		for (const opt of options) {
 			const isDisabled = opt.value === e.target.value;
 			opt.disabled = isDisabled;
 			if (opt.selected && isDisabled) {
 				opt.selected = false;
 			}
-		});
+		}
 	});
 })();
