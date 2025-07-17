4248351f0ba99d56ad87a2e40211b8b6be01cf2d
Raphael Stefanini <raphael@rphlo.com>
2025-07-17 08:23:34 +0300

    remove dead code

--- routechoices/lib/slippy_tiles.py
+++ routechoices/lib/slippy_tiles.py
@@ -9,16 +9,6 @@ def latlon_to_tile_xy(lat_deg, lon_deg, zoom):
     return (xtile, ytile)
 
 
-def latlon_to_tile_xy_offset(lat_deg, lon_deg, zoom):
-    lat_rad = math.radians(lat_deg)
-    n = 2.0**zoom
-    xx = (lon_deg + 180.0) / 360.0 * n
-    yy = (1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n
-    xoff = int((xx % 1.0) * 256)
-    yoff = int((yy % 1.0) * 256)
-    return (xoff, yoff)
-
-
 def tile_xy_to_north_west_latlon(xtile, ytile, zoom):
     n = 2.0**zoom
     lon_deg = xtile / n * 360.0 - 180.0
