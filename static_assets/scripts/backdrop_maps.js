e8e843414280c72c361a34dac52bce26b9ab4029
Raphael Stefanini <raphael@rphlo.com>
2025-10-14 06:35:16 +0300

    mapanturl

--- static_assets/scripts/backdrop_maps.js
+++ static_assets/scripts/backdrop_maps.js
@@ -35,7 +35,7 @@ const backdropMaps = {
 		},
 	),
 	"mapant-fi": L.tileLayer(
-		"https://www.mapant.fi/wmts/wmts_EPSG3857.php?z={z}&x={x}&y={y}",
+		"https://wmts.mapant.fi/wmts_EPSG3857.php?z={z}&x={x}&y={y}",
 		{
 			attribution: "&copy; MapAnt.fi and National Land Survey of Finland",
 			className: "wms256",
