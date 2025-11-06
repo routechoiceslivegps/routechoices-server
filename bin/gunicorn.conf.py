910b920fd2b1a34d132b920f95cba208299612c7
Raphael Stefanini <raphael@rphlo.com>
2025-11-06 17:53:42 +0200

    more resources

--- bin/gunicorn.conf.py
+++ bin/gunicorn.conf.py
@@ -2,7 +2,7 @@ wsgi_app = "routechoices.wsgi:application"
 preload_app = True
 daemon = False
 raw_env = ["DJANGO_SETTINGS_MODULE=routechoices.settings"]
-workers = 4
-threads = 3
+workers = 5
+threads = 4
 max_requests = 3000
 max_requests_jitter = 100
