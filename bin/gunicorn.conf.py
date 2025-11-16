caf07f7f623245f1eb7a06383ec1aaa77713f8d7
Raphael Stefanini <raphael@rphlo.com>
2025-11-16 11:33:42 +0200

    Multiple django service

--- bin/gunicorn.conf.py
+++ bin/gunicorn.conf.py
@@ -2,7 +2,7 @@ wsgi_app = "routechoices.wsgi:application"
 preload_app = True
 daemon = False
 raw_env = ["DJANGO_SETTINGS_MODULE=routechoices.settings"]
-workers = 5
-threads = 4
+workers = 3
+threads = 3
 max_requests = 3000
 max_requests_jitter = 100
