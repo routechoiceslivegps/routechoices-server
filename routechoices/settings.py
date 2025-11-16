f8254c5fe5526b1c5d06686ab110e19e7baad18d
Raphael Stefanini <raphael@rphlo.com>
2025-11-16 20:01:58 +0200

    Improve oauth

--- routechoices/settings.py
+++ routechoices/settings.py
@@ -224,7 +224,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "oauth2_provider.backends.OAuth2Backend",
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
