bb3d1b2e8c4c6602479706c44efbc89247c79d5b
Rapha <rphlo@users.noreply.github.com>
2025-11-18 00:50:58 +0200

    Update urls.py

--- routechoices/mapdump/urls.py
+++ routechoices/mapdump/urls.py
@@ -5,5 +5,5 @@ from routechoices.mapdump import views
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     re_path(r"^map/(?P<aid>[0-9a-zA-Z_-]+)/?", views.effort_view, name="effort_view"),
-    re_path(r"^(?P<username>[0-9a-zA-Z_-]{2,})/?$", views.user_view, name="user_view"),
+    re_path(r"^(?P<username>[0-9a-zA-Z_-]+)/?$", views.user_view, name="user_view"),
 ]
